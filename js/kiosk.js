939f7eca8aae315cb444d524b1e52d0b45d435c9
John Haney <geojwh@gmail.com>
2018-07-10 15:31:10 -0500

    url fixes, app additions, bootstrap grid changes

--- js/kiosk.js
+++ js/kiosk.js
@@ -10,29 +10,19 @@ const switchNav = () => {
   }
 }
 
-const appArray = [
-  {id:'flood', url:'https://map.texasflood.org'},
-  {id:'swp', url:'https://2017.texasstatewaterplan.org'},
-  {id:'meso', url:'http://texmesonet.org'},
-  {id:'twdb-site', url:'http://www.twdb.texas.gov'},
-  {id:'tnris', url:'https://tnris.org'},
-  {id:'water-data', url:'https://waterdatafortexas.org'}
-]
-
 const appObj = {
   'flood':'https://map.texasflood.org',
   'swp':'https://2017.texasstatewaterplan.org',
-  'meso':'http://texmesonet.org',
-  'twdb-site':'http://www.twdb.texas.gov',
+  'meso':'https://www.texmesonet.org/Viewer/?',
+  'twdb-site':'https://www.twdb.texas.gov',
   'tnris':'https://tnris.org',
-  'water-data':'https://waterdatafortexas.org'
+  'water-data':'https://waterdatafortexas.org',
+  'water-data-interact':'https://www2.twdb.texas.gov/apps/waterdatainteractive/groundwaterdataviewer',
+  'water-hydro':'https://waterdatafortexas.org/coastal/hydrology'
 }
 
 const switchToIframe = (e) => {
-  console.log(e);
-
   const url = appObj[e];
-  console.log(url);
 
   const window = document.createElement("iframe");
   window.src = url;
