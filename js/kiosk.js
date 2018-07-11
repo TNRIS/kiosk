ddcc9f0775d73ab987d285631b076fa476bd291d
John Haney <geojwh@gmail.com>
2018-07-11 15:21:20 -0500

    added logos; fixed whitespace at bottom of iframes;
    also working on js for refresh when inactive.

--- js/kiosk.js
+++ js/kiosk.js
@@ -1,3 +1,27 @@
+// refresh page every 5 min unless active
+let idleTime = 0;
+
+const timerIncrement = () => {
+  idleTime = idleTime + 1;
+  if (idleTime > 10) { // 20 minutes
+    window.location.reload();
+  }
+}
+
+$(document).ready(function () {
+  //Increment the idle time counter every second.
+  const idleInterval = setInterval(timerIncrement, 1000); // 1 second
+
+  //Zero the idle timer on mouse movement.
+  $(this).mousemove(function (e) {
+      idleTime = 0;
+  });
+  $(this).keypress(function (e) {
+      idleTime = 0;
+  });
+});
+
+// switch bottom navigation bar and set card menu to display:none
 const switchNav = () => {
   const twdbName = document.getElementById("twdb-name");
   const backToMenu = document.getElementById("back-to-menu");
@@ -10,6 +34,7 @@ const switchNav = () => {
   }
 }
 
+// object of app urls to be used in switchToIframe function
 const appObj = {
   'flood':'https://map.texasflood.org',
   'swp':'https://2017.texasstatewaterplan.org',
@@ -21,6 +46,7 @@ const appObj = {
   'water-hydro':'https://waterdatafortexas.org/coastal/hydrology'
 }
 
+// create iframe using app url from appObj and append to body
 const switchToIframe = (e) => {
   const url = appObj[e];
   const window = document.createElement("iframe");
