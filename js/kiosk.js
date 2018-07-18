702416415edbbebecad45cf8386bf30191af0bea
John Haney <geojwh@gmail.com>
2018-07-18 16:00:06 -0500

    back button work; nav switch with home button and back arrow

--- js/kiosk.js
+++ js/kiosk.js
@@ -22,26 +22,48 @@ $(document).ready(function () {
   });
 });
 
+function goBack() {
+  window.history.back();
+  const iframeWindow = document.getElementById("window");
+  console.log(iframeWindow.contentWindow.location.href);
+  if (iframeWindow.contentWindow.location.href === "https://tnris.org/") {
+    console.log("tnris.org");
+  }
+}
+
 // switch bottom navigation bar and set card menu to display:none
 const switchNav = () => {
   const navDiv = document.getElementById("twdb-name");
   const cardMenu = document.getElementById("card-menu");
   const backLink = document.createElement("a");
-  const backText = document.createTextNode(" Back");
-  const backImage = document.createElement("img");
+  // const backText = document.createTextNode(" Back");
+  const backArrow = document.createElement("i");
+  const home = document.createElement("i");
+  const brandImage = document.getElementById("brand-image");
+  const navLink = document.getElementById("nav-link");
+
+  // replace twdb logo with home button
+  brandImage.parentNode.removeChild(brandImage);
+  navLink.setAttribute("href", "/");
+  home.setAttribute("class", "fa fa-2x fa-home");
 
   // remove card menu for app iframe
   cardMenu.style.display = "none";
+
   // insert href link for back nav option & class for text style
-  backLink.setAttribute("href", "javascript:history.back()");
+  backLink.setAttribute("onclick", "goBack()");
+  // backLink.setAttribute("href", "javascript:window.history.back()");
   backLink.setAttribute("class", "navbar-brand nav-text");
-  // set back image attributes
-  backImage.setAttribute("src", "/css/img/back.png");
-  backImage.setAttribute("class", "back-image");
+
+  // set back arrow class attribute for fa
+  backArrow.setAttribute("class", "fa fa-2x fa-angle-double-left");
+
   // append back nav elemnts to div
-  backLink.appendChild(backImage);
-  backLink.appendChild(backText);
+  navLink.appendChild(home);
+  backLink.appendChild(backArrow);
+  // backLink.appendChild(backText);
   navDiv.appendChild(backLink);
+
 }
 
 // object of app urls to be used in switchToIframe function
